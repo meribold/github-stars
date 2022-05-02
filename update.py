e5b533b54962f68d96e284fac4a1c7b0a1450577
Lukas Waymann <git@meribold.org>
2022-05-02 16:56:35 +0800

    Automatically update the list using GitHub Actions
    https://github.com/maguowei/awesome-stars/blob/master/.github/workflows/schedules.yml
    https://github.com/features/actions
    https://docs.github.com/en/actions
    https://docs.github.com/en/actions/security-guides/automatic-token-authentication
    https://docs.github.com/en/actions/security-guides/automatic-token-authentication#permissions-for-the-github_token
    https://docs.github.com/en/actions/security-guides/automatic-token-authentication#modifying-the-permissions-for-the-github_token
    https://github.com/meribold/github-stars/settings/actions
    https://github3.readthedocs.io
    https://en.wikipedia.org/wiki/YAML

--- update.py
+++ update.py
@@ -1,6 +1,9 @@
+import os
 from collections import defaultdict
+
 from github3 import GitHub
 
+gh = GitHub(token=os.environ["GITHUB_TOKEN"])
 
 html_escape_table = {
     ">": "&gt;",
@@ -14,31 +17,32 @@ def html_escape(text):
 
 repo_list_dict = defaultdict(list)
 
-for s in GitHub().starred_by("meribold"):
+for s in gh.starred_by("meribold"):
     repo_list_dict[s.language or "Miscellaneous"].append(
         [s.full_name, s.html_url, html_escape((s.description or "").strip())]
     )
 
 repo_list_of_lists = sorted(repo_list_dict.items(), key=lambda r: r[0])
 
-print("## Languages\n")
-
-print(
-    *(
+new_readme_content = (
+    "## Languages\n\n"
+    + "\n".join(
         "*   [{}](#{})".format(language, "-".join(language.lower().split()))
         for language, _ in repo_list_of_lists
-    ),
-    sep="\n",
-)
-
-print(
-    *(
+    )
+    + "\n"
+    + "\n".join(
         f"\n## {language}\n\n"
         + "\n".join(
             f"*   [{name}]({url})" + (f": {desc}" if desc else "")
             for name, url, desc in repo_list
         )
         for language, repo_list in repo_list_of_lists
-    ),
-    sep="\n",
-)
+    )
+    + "\n"
+).encode()
+
+readme = gh.repository("meribold", "github-stars").readme()
+
+if new_readme_content != readme.decoded:
+    readme.update("Update list of starred repositories", new_readme_content)
