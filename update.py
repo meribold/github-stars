7a7a56f76d725e71c4b584bd2c46101926c98c09
Lukas Waymann <git@meribold.org>
2022-05-07 16:48:38 +0800

    WIP
    https://github.com/meribold/github-stars/settings/actions
    https://docs.github.com/en/actions/security-guides/automatic-token-authentication
    https://docs.github.com/en/actions/security-guides/automatic-token-authentication#permissions-for-the-github_token
    https://docs.github.com/en/actions/security-guides/automatic-token-authentication#modifying-the-permissions-for-the-github_token

--- update.py
+++ update.py
@@ -1,6 +1,9 @@
+import os
 from collections import defaultdict
+
 from github3 import GitHub
 
+gh = GitHub(token=os.environ["GITHUB_TOKEN"])
 
 html_escape_table = {
     ">": "&gt;",
@@ -14,31 +17,30 @@ def html_escape(text):
 
 repo_list_dict = defaultdict(list)
 
-for s in GitHub().starred_by("meribold"):
+for s in gh.starred_by("meribold"):
     repo_list_dict[s.language or "Miscellaneous"].append(
         [s.full_name, s.html_url, html_escape((s.description or "").strip())]
     )
 
 repo_list_of_lists = sorted(repo_list_dict.items(), key=lambda r: r[0])
 
-print("## Languages\n")
-
-print(
-    *(
-        "*   [{}](#{})".format(language, "-".join(language.lower().split()))
-        for language, _ in repo_list_of_lists
-    ),
-    sep="\n",
-)
-
-print(
-    *(
-        f"\n## {language}\n\n"
+gh.repository("meribold", "github-stars").readme().update(
+    "Update list of starred repositories",
+    (
+        "## Languages\n\n"
         + "\n".join(
-            f"*   [{name}]({url})" + (f": {desc}" if desc else "")
-            for name, url, desc in repo_list
+            "*   [{}](#{})".format(language, "-".join(language.lower().split()))
+            for language, _ in repo_list_of_lists
         )
-        for language, repo_list in repo_list_of_lists
-    ),
-    sep="\n",
+        + "\n"
+        + "\n".join(
+            f"\n## {language}\n\n"
+            + "\n".join(
+                f"*   [{name}]({url})" + (f": {desc}" if desc else "")
+                for name, url, desc in repo_list
+            )
+            for language, repo_list in repo_list_of_lists
+        )
+        + "\n"
+    ).encode(),
 )
