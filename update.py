94758ffa38d6d3e6be1cf422b8f04d8609d82c15
Lukas Waymann <git@meribold.org>
2022-05-08 11:23:39 +0800

    Snapshot
    This is a snapshot commit created by git-snap.
    
    https://github.com/meribold/git-snap

--- update.py
+++ update.py
@@ -24,23 +24,25 @@ for s in gh.starred_by("meribold"):
 
 repo_list_of_lists = sorted(repo_list_dict.items(), key=lambda r: r[0])
 
-gh.repository("meribold", "github-stars").readme().update(
-    "Update list of starred repositories",
-    (
-        "## Languages\n\n"
+new_readme_content = (
+    "## Languages\n\n"
+    + "\n".join(
+        "*   [{}](#{})".format(language, "-".join(language.lower().split()))
+        for language, _ in repo_list_of_lists
+    )
+    + "\n"
+    + "\n".join(
+        f"\n## {language}\n\n"
         + "\n".join(
-            "*   [{}](#{})".format(language, "-".join(language.lower().split()))
-            for language, _ in repo_list_of_lists
+            f"*   [{name}]({url})" + (f": {desc}" if desc else "")
+            for name, url, desc in repo_list
         )
-        + "\n"
-        + "\n".join(
-            f"\n## {language}\n\n"
-            + "\n".join(
-                f"*   [{name}]({url})" + (f": {desc}" if desc else "")
-                for name, url, desc in repo_list
-            )
-            for language, repo_list in repo_list_of_lists
-        )
-        + "\n"
-    ).encode(),
-)
+        for language, repo_list in repo_list_of_lists
+    )
+    + "\n"
+).encode()
+
+readme = gh.repository("meribold", "github-stars").readme()
+
+if new_readme_content != readme.decoded:
+    readme.update("Update list of starred repositories", new_readme_content)
